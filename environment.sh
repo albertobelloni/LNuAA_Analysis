18bec0326f12de15f45c63f6031554b885431e9d
Lindsey Gray <lindsey.gray@gmail.com>
2014-01-27 19:41:30 +0100

    update analysis inputs and format output better for stack plotter

--- environment.sh
+++ environment.sh
@@ -2,6 +2,6 @@
 
 source ${CMSSW_BASE}/src/FinalStateAnalysis/environment.sh
 export LNuAA_ANALYSIS_DIR="Wgamgam"
-export LNuAA_ANALYSIS_REVISION="FilteredSamplesJan14"
+export LNuAA_ANALYSIS_REVISION="FilteredSamplesFeb14"
 
 export MIN_PROXY_HOURS=24
